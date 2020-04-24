55245ddc49ef614b21613657852553ae12fda618
Feng Wei <fwei95@uw.edu>
2020-04-24 16:43:34 -0700

    Xylophone completed

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,35 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        print ("Start")
+        sender.alpha = 0.5
+        let seconds = 0.2
+        DispatchQueue.main.asyncAfter(deadline: .now() + seconds) {
+            sender.alpha = 1
+        }
+    }
     
-
+    
+    @IBAction func keyReleased(_ sender: UIButton) {
+        print(sender.currentTitle! + " is released.")
+        playSound(key: sender.currentTitle!)
+    }
+    
+    func playSound(key: String) {
+        let url = Bundle.main.url(forResource: key, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
-
